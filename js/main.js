2a3d92c41cf2e464dedb7bc355d03c8bbd952b66
Michael Tang <tngmichael28@gmail.com>
2022-01-06 23:19:29 -0800

    Calculator is functional. User can exit out of calc modal and calc result modal.

--- js/main.js
+++ js/main.js
@@ -4,39 +4,62 @@
 // var $ohpMax = document.getElementById('ohp-max');
 var $maxCalculator = document.getElementById('open-calc');
 var $calcModal = document.getElementById('calc-modal');
-var $closeIcon = document.querySelector('.fa-times');
+var $closeCalc = document.getElementById('close-calc');
 var $calcMaxForm = document.getElementById('calc-max');
+var $maxResult = document.getElementById('max-result');
+var $maxResultModal = document.getElementById('max-result-modal');
+var $closeCalcResult = document.getElementById('close-calc-result');
+var $exitCalcResult = document.getElementById('exit-calc-result');
 
 function openModal(event) {
-  if (event.target.id === 'open-calc') {
+  var temp = null;
+  try {
+    temp = event.target.id;
+  } catch (error) {
+    temp = event.id;
+  }
+  if (temp === 'open-calc') {
     $calcModal.style.display = 'block';
+  }
+  if (temp === 'max-result-modal') {
+    $maxResultModal.style.display = 'block';
   }
 }
 
 function closeModal(event) {
-  if ($calcModal.style.display === 'block') {
-    $calcModal.style.display = '';
+  var close = event.target;
+  while (close.style.display !== 'block') {
+    close = close.parentElement;
   }
+  close.style.display = '';
 }
 
 function calculateMax(event) {
   event.preventDefault();
   var formData = new FormData($calcMaxForm);
-  if (isNaN(formData.get('calc-weight'))) {
+  var calcWeight = formData.get('calc-weight');
+  var calcRep = formData.get('calc-rep');
+  var max = Number(calcWeight) * (1 + Number(calcRep) / 30);
+  if (isNaN(calcWeight)) {
     alert('Please input a positive number for Weight');
     return;
-  } else if (Number(formData.get('calc-weight') < 1)) {
+  } else if (Number(calcWeight) < 1) {
     alert('Please input a positive number for Weight');
     return;
   }
-  if (isNaN(formData.get('calc-rep'))) {
+  if (isNaN(calcRep)) {
     alert('Please input a positive number for Reps');
 
-  } else if (Number(formData.get('calc-rep') < 1)) {
+  } else if (Number(calcRep) < 1) {
     alert('Please input a positive number for Rep');
   }
+  $maxResult.textContent = max - (max % 5) + formData.get('weight-meas-type');
+  closeModal(event);
+  openModal($maxResultModal);
 }
 
 $maxCalculator.addEventListener('click', openModal);
-$closeIcon.addEventListener('click', closeModal);
+$closeCalc.addEventListener('click', closeModal);
 $calcMaxForm.addEventListener('submit', calculateMax);
+$closeCalcResult.addEventListener('click', closeModal);
+$exitCalcResult.addEventListener('click', closeModal);
