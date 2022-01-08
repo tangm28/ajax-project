0afb8e0c72e92414fd3096d449259534265c6c7c
Michael Tang <tngmichael28@gmail.com>
2022-01-07 16:49:55 -0800

    Submit event will save maxes to local storage and user need to input positive number otherwise an alert will come up.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
-/* exported data */
+/* exported maxes */
+var previousMaxes;
+
+var maxes = {
+  maxesTrend: [],
+  todayDate: new Date(),
+  nextTrendId: 1
+};
+
+function saveDataB4Unload(event) {
+  localStorage.setItem('maxes-local-storage', JSON.stringify(maxes));
+}
+previousMaxes = localStorage.getItem('maxes-local-storage');
+if (previousMaxes !== null) {
+  maxes.maxesTrend = JSON.parse(previousMaxes).maxesTrend;
+  maxes.nextTrendId = JSON.parse(previousMaxes).nextTrendId++;
+}
+window.addEventListener('beforeunload', saveDataB4Unload);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* global maxes */
 // var $benchMax = document.getElementById('bench-max');
 // var $squatMax = document.getElementById('squat-max');
 // var $deadliftMax = document.getElementById('deadlift-max');
@@ -10,6 +11,7 @@ var $maxResult = document.getElementById('max-result');
 var $maxResultModal = document.getElementById('max-result-modal');
 var $closeCalcResult = document.getElementById('close-calc-result');
 var $exitCalcResult = document.getElementById('exit-calc-result');
+var $dataMaxes = document.getElementById('data-maxes');
 
 function openModal(event) {
   var temp = null;
@@ -58,8 +60,63 @@ function calculateMax(event) {
   openModal($maxResultModal);
 }
 
+function saveMaxes(event) {
+  event.preventDefault();
+  var formData = new FormData($dataMaxes);
+  var benchMax = formData.get('bench-max');
+  var benchMeas = formData.get('bench-meas-type');
+  var squatMax = formData.get('squat-max');
+  var squatMeas = formData.get('squat-meas-type');
+  var deadliftMax = formData.get('deadlift-max');
+  var deadliftMeas = formData.get('deadlift-meas-type');
+  var ohpMax = formData.get('ohp-max');
+  var ohpMeas = formData.get('ohp-meas-type');
+  if (isNaN(benchMax)) {
+    alert('Please input a positive number for Bench');
+    return;
+  } else if (Number(benchMax) < 1) {
+    alert('Please input a positive number for Bench');
+    return;
+  }
+  if (isNaN(squatMax)) {
+    alert('Please input a positive number for Squat');
+    return;
+  } else if (Number(squatMax) < 1) {
+    alert('Please input a positive number for Squat');
+    return;
+  }
+  if (isNaN(deadliftMax)) {
+    alert('Please input a positive number for Deadlift');
+    return;
+  } else if (Number(deadliftMax) < 1) {
+    alert('Please input a positive number for Deadlift');
+    return;
+  }
+  if (isNaN(ohpMax)) {
+    alert('Please input a positive number for Overhead Press');
+
+  } else if (Number(ohpMax) < 1) {
+    alert('Please input a positive number for Overhead Press');
+  }
+  var currentMaxes = {
+    maxBench: benchMax,
+    benchMeas: benchMeas,
+    maxSquat: squatMax,
+    squatMeas: squatMeas,
+    maxDeadlift: deadliftMax,
+    deadliftMeas: deadliftMeas,
+    maxOHP: ohpMax,
+    ohpMeas: ohpMeas,
+    today: new Date(),
+    nextTrendId: maxes.nextTrendId
+  };
+  maxes.nextTrendId++;
+  maxes.maxesTrend.unshift(currentMaxes);
+}
+
 $maxCalculator.addEventListener('click', openModal);
 $closeCalc.addEventListener('click', closeModal);
 $calcMaxForm.addEventListener('submit', calculateMax);
 $closeCalcResult.addEventListener('click', closeModal);
 $exitCalcResult.addEventListener('click', closeModal);
+$dataMaxes.addEventListener('submit', saveMaxes);
